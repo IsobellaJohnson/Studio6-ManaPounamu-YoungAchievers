0be7475e916561bd1d37b47e020e760d9e61904b
IsobellaJohnson <64507442+IsobellaJohnson@users.noreply.github.com>
2022-08-23 12:27:00 +1200

    form

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
   <div className="App">
     <div className="container">
       <form>
+      <div className="si-title">Student Information</div>
       <div className="row">
     <div className="col">
-      <label htmlFor="nominatingSchool" className="form-label">Nominating School*</label>
+      <label htmlFor="nominatingSchool" className="form-label">Nominating School *</label>
       <input type="text" className="form-control"></input>
     </div>
       <div className="col">
-      <label htmlFor="level" className="form-label">Level*</label>
+      <label htmlFor="level" className="form-label">Level *</label>
     <select class="form-select" aria-label="Default select example">
       <option selected>-- Select a level --</option>
       <option value="1">Teina/Junior</option>
@@ -20,7 +21,7 @@ function App() {
     </select>
     </div>
     <div className="col">
-    <label htmlFor="year" className="form-label">Year*</label>
+    <label htmlFor="year" className="form-label">Year *</label>
     <select class="form-select" aria-label="Default select example">
       <option selected>-- Select a year --</option>
       <option value="1">Year 9</option>
@@ -33,24 +34,127 @@ function App() {
     </div>
     <div className="row">
   <div className="col">
-    <label htmlFor="exampleInputPassword1" className="form-label">Student Name*</label>
+    <label htmlFor="exampleInputPassword1" className="form-label">Student Name *</label>
     <input type="text" className="form-control" id="exampleInputPassword1"></input>
   </div>
   <div className="col">
-    <label htmlFor="exampleInputPassword1" className="form-label">Iwi*</label>
+    <label htmlFor="exampleInputPassword1" className="form-label">Iwi *</label>
     <input type="text" className="form-control" id="exampleInputPassword1"></input>
   </div>
   </div>
   <div className="row">
     <div className="col-md-4">
-    <label htmlFor="exampleInputPassword1" className="form-label">Student Photo*</label>
+    <label htmlFor="exampleInputPassword1" className="form-label">Student Photo *</label>
     <div class="input-group mb-3">
   <label class="input-group-text" for="inputGroupFile01">Upload</label>
   <input type="file" class="form-control" id="inputGroupFile01"></input>
-</div>
-    </div>
   </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
+  </div>
+  </div>
+
+  <div className="si-title">Student Achievements</div>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Whakauru i te iwi whānui / Community or cultural involvement *</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="6" placeholder='For example:&#x0a;* Aroha ki kā takata - show manaaki to people at school, wider community&#x0a;* Kapa Haka member/Kaea&#x0a;* Participation in numerous whānau/community hui&#x0a;* Member of the Orchestra&#x0a;* Choir member'></textarea>
+  </div>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Te ārahina / Leadership *</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="7" placeholder='For example:&#x0a;* Attendance at Hui Taihoi&#x0a;* Head boy/Girl&#x0a;* Prefect&#x0a;* Peer Support&#x0a;* Tū Mai Rakatahi&#x0a;* Ngā Manu Kо̄rero Representative'></textarea>
+  </div>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Mātauraka / Academic *</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="7" placeholder='For example:&#x0a;* Australasian English Competitions&#x0a;* Hands-on at Otago&#x0a;* Please list subject/cirriculum strengths'></textarea>
+  </div>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Hākinakina / Sporting *</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="6" placeholder='For example:&#x0a;* Representative&#x0a;* Captain&#x0a;* Sporting Blues&#x0a;* Participation&#x0a;* Coaching'></textarea>
+  </div>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">He kōrero tautoko anō / Other supporting information not previously covered</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='For example:&#x0a;* Please tell us about your future plans'></textarea>
+  </div>
+
+  <div className="si-title">Mana Pounamu Young Achiever Recipient Testimonial</div>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Nominee Testimonial*</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+  </div>
+  <div className="row">
+    <div className="col">
+      <label htmlFor="Nominee" className="form-label">Nominee*</label>
+      <input type="text" className="form-control"></input>
+    </div>
+    <div className="col">
+      <label htmlFor="Nominee" className="form-label">Nominee Position*</label>
+      <input type="text" className="form-control"></input>
+    </div>
+    </div>
+
+    <div className="si-title">Mana Pounamu Young Achiever Recipient Whānau Endorsement</div>
+  <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Whanau Endorsement *</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+  </div>
+  <div className="row">
+    <div className="col">
+      <label htmlFor="Nominee" className="form-label">Endorser Name *</label>
+      <input type="text" className="form-control"></input>
+    </div>
+    <div className="col">
+      <label htmlFor="Nominee" className="form-label">Endorser Relationship *</label>
+      <input type="text" className="form-control"></input>
+    </div>
+    </div>
+
+    <div className="si-title">Mana Pounamu Media Consent</div>
+    <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+  <label class="form-check-label" for="flexCheckDefault">
+  I give permission for Mana Pounamu to use my photo, artwork, name, and/or profile for promotional purposes, in a promotional video/CDRom, fliers, the world wide web and in any other media items aimed at promoting Mana Pounamu, for a period of five years, unless otherwise stated. *
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked"></input>
+  <label class="form-check-label" for="flexCheckChecked">
+  Mana Pounamu will always use discretion when using video footage, my photo, artwork and/or profile and will not use it to discredit me in any way. *
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked"></input>
+  <label class="form-check-label" for="flexCheckChecked">
+  I understand that I have the right to update or delete my file at any time. Once this request is made, it will be applicable to all future productions – all existing \ promotions will be continued to be used. *
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked"></input>
+  <label class="form-check-label" for="flexCheckChecked">
+  I understand participants who are under 18 years of age have consent by their parent or guardian in accepting the above release. *
+  </label>
+</div>
+
+<div className="si-title">Providers Details</div>
+<div className="row">
+    <div className="col">
+      <label htmlFor="Nominee" className="form-label">Provider Name *</label>
+      <input type="text" className="form-control"></input>
+    </div>
+    <div className="col">
+      <label htmlFor="Nominee" className="form-label">Provider Email *</label>
+      <input type="text" className="form-control"></input>
+    </div>
+    <div className="col">
+      <label htmlFor="Nominee" className="form-label">Provider Phone</label>
+      <input type="text" className="form-control"></input>
+    </div>
+    </div>
+    <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked"></input>
+  <label class="form-check-label" for="flexCheckChecked">
+  I understand participants who are under 18 years of age have consent by their parent or guardian in accepting the above release. *
+  </label>
+</div>
+
+  <button type="submit" className="btn btn-primary sub-btn">Submit</button>
   </form>
     </div>
   </div>
